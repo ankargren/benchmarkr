32e7393ff1221847f2331a7a0b80215e16df08fe
ankargren <sebastian.ankargren@statistik.uu.se>
2020-11-19 19:45:09 +0100

    Added plot function

--- R/benchmarkr.R
+++ R/benchmarkr.R
@@ -1,7 +1,8 @@
-mvn_generation <- function(times, dimension) {
-  mu <- rep(0, dimension)
-  Sigma <- crossprod(matrix(rnorm(dimension*10000), 10000, dimension))/10000
+mvn_generation <- function(times, n) {
+  mu <- rep(0, n)
+  Sigma <- crossprod(matrix(rnorm(n*10000), 10000, n))/10000
   mb <- microbenchmark::microbenchmark(MASS::mvrnorm(n = 1, mu = mu, Sigma = Sigma), times = times)
+  mb$n <- n
   return(mb)
 }
 
@@ -9,12 +10,15 @@ lm_estimation <- function(times, n, p) {
   X <- matrix(rnorm(n*p), n, p)
   y <- matrix(rnorm(n), n, 1)
   mb <- microbenchmark::microbenchmark(lm(y~X), times = times)
+  mb$n <- n
+  mb$p <- p
   return(mb)
 }
 
 density_evaluation <- function(times, n) {
   x <- rnorm(n)
   mb <- microbenchmark::microbenchmark(dnorm(x), times = times)
+  mb$n <- n
   return(mb)
 }
 
@@ -33,22 +37,58 @@ mcmc_sampling <- function(times, n) {
   }
 
   mb <- microbenchmark::microbenchmark(mcmc_temporary_fun(n, mu, Sigma), times = times)
+  mb$n <- n
   return(mb)
 }
-benchmark <- function(times = 100, mvn_dimension = 100, lm_n = 1000, lm_p = 50,
+benchmark <- function(times = 100, mvn_n = 100, lm_n = 1000, lm_p = 50,
                       density_n = 100000, mcmc_n = 1000) {
-  mb_mvn_generation <- mvn_generation(times, dimension = mvn_dimension)
+  mb_mvn_generation <- mvn_generation(times, n = mvn_n)
   mb_lm_estimation <- lm_estimation(times, n = lm_n, p = lm_p)
   mb_density_evaluation <- density_evaluation(times, n = density_n)
   mb_mcmc_sampling <- mcmc_sampling(times, n = mcmc_n)
-  return(list(
+
+  mb_list <- list(
     mvn_generation = mb_mvn_generation,
     lm_estimation = mb_lm_estimation,
     density_evaluation = mb_density_evaluation,
     mcmc_sampling = mb_mcmc_sampling
-    ))
+  )
+  mb_vec <- sapply(mb_list, function(x) median(x$time))
+  class(mb_vec) <- "benchmarkr"
+  return(mb_vec)
+}
+
+
+
+comparisons <- function() {
+  comp1 <- data.frame(
+    step = c("mvn_generation", "lm_estimation", "density_evaluation", "mcmc_sampling"),
+    time = c(2564724, 4219191, 2319384, 3855403),
+    computer = rep("Sebastian's Macbook Pro (2017)")
+  )
+
+  # Add more here
+
+  comp <- rbind(comp1)
+  return(comp)
+}
+
+
+plot.benchmarkr <- function(x, ...) {
+  comp <- comparisons()
+  current <- data.frame(step = names(x), time = c(x), computer = "Current")
+  plot_df <- rbind(comp, current)
+  p <- ggplot(plot_df, aes(x = step, y = time/1e6, fill = computer)) +
+    geom_bar(position="dodge", stat="identity") +
+    theme_minimal() +
+    labs(y = "Milliseconds",
+         x = "Benchmarking step",
+         fill = "Computer") +
+    theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=0.5))
+  return(p)
 }
 
 # Example
-bm <- benchmark()
-milliseconds_medians <- sapply(bm, function(x) median(x$time)/1e6)
+#bm <- benchmark()
+
+#plot_fun(bm)
